2c7a53b7d4cc9031995a790c71c7b275cab67994
Lux <luis.stocel4193@itse.ac.pa>
2026-04-12 13:30:17 -0500

    Creacion del login y dashboard por Lloyd
    Co-authored-by: Isaac <isacito924@gmail.com>
    Co-authored-by: luisadhonys-lab <luisadhonys@gmail.com>

--- Login_signIn/src/app/components/dashboard/dashboard.ts
+++ Login_signIn/src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+   constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
