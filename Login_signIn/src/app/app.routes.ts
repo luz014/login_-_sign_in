63b269ca40a1b1c3f5d561e872d27e557752162d
Lux <luis.stocel4193@itse.ac.pa>
2026-04-12 14:13:06 -0500

    Creacion del login y dashboard por Lloyd (#4)
    Co-authored-by: Isaac <isacito924@gmail.com>
    Co-authored-by: luisadhonys-lab <luisadhonys@gmail.com>

--- Login_signIn/src/app/app.routes.ts
+++ Login_signIn/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
+import { authGuardGuard } from './guards/auth-guard-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard, canActivate: [AuthGuard] }
+];
+
--- Login_signIn/src/app/components/shared/header/header.ts
+++ Login_signIn/src/app/components/shared/header/header.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +8,9 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  menu = [
+    { url: "/", content: "HOME"},
+    { url: "/register", content: "Registrarse"}
+  ]
+}
