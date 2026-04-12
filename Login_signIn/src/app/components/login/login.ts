be0320b610f6722d440dc469e0b0fa8c6f8390a0
luisadhonys-lab <luisadhonys@gmail.com>
2026-04-12 12:41:18 -0500

    Creacion del login y dashboard

--- Login_signIn/src/app/components/login/login.ts
+++ Login_signIn/src/app/components/login/login.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    const success = this.authService.login(this.email, this.password);
+
+    if (success) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.errorMessage = 'Credenciales incorrectas';
+    }
+  }
+}
