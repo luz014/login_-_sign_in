4ef756051e44d155a2e98837b89970ec2c03273f
Lux <luis.stocel4193@itse.ac.pa>
2026-04-13 10:32:11 -0500

    Agregado del scss y mejoras arreglos del html (#16)

--- Login_signIn/src/app/components/dashboard/dashboard.ts
+++ Login_signIn/src/app/components/dashboard/dashboard.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth-service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/user-model';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
-   constructor(private authService: AuthService, private router: Router) {}
+
+export class Dashboard implements OnInit {
+  user : User | null = null;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.getCurrentUser();}
 
   logout() {
     this.authService.logout();
-    this.router.navigate(['/login']);
+    this.router.navigate(['/login']); 
   }
 }
--- Login_signIn/src/app/components/register/register.ts
+++ Login_signIn/src/app/components/register/register.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth-service';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/user-model';
+
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ ReactiveFormsModule, CommonModule ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
+
 export class Register {
 
   registerForm: FormGroup;
   submitted = false;
+  success = '';
+  error = '';
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.registerForm = this.fb.group({
-      name: ['', Validators.required],
+      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
@@ -22,17 +28,35 @@ export class Register {
 
   onSubmit() {
     this.submitted = true;
+    this.success = '';
+    this.error = '';
 
-    if (this.registerForm.invalid) return;
+    if (this.registerForm.invalid) {
+      return;
+    }
 
-    this.authService.register(this.registerForm.value);
-    console.log('Usuario registrado:', this.registerForm.value);
+    const newUser: User = {
+      id: 0,
+      username: this.registerForm.value.username,
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password
+    };
+
+    const success = this.authService.register(newUser);
+
+    if (!success) {
+      this.error = 'Un error ha ocurrido, vuelva a hacerlo';
+      return;
+    }
+
+    this.success = `Te has registrado yei, ${newUser.username}`;
 
     this.registerForm.reset();
     this.submitted = false;
-  }  
-
+  }
+  
   get f() {
     return this.registerForm.controls;
+
   }
 }
