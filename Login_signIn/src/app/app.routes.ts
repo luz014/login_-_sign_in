5cee2d86d82cf40cb173cf2dcfdfe366c111a988
Lux <luis.stocel4193@itse.ac.pa>
2026-04-13 06:29:53 -0500

    Integrar service y auth (#6)

--- Login_signIn/src/app/app.routes.ts
+++ Login_signIn/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Dashboard } from './components/dashboard/dashboard';
 import { authGuardGuard } from './guards/auth-guard-guard';
+import { Register } from './components/register/register';
 
 export const routes: Routes = [
-      { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard, canActivate: [AuthGuard] }
+  { path: 'register', component: Register },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuardGuard] },
+  { path: '**', redirectTo: 'login' }
 ];
 
